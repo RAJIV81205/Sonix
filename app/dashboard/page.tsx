ae0354796891fb0db787ad8f4e78cd8900c9bd55
Rajiv <lucky81205@gmail.com>
2025-04-30 17:55:37 +0530

    feat: update Dashboard layout for improved structure and styling

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,10 @@
 
 const DashboardPage = () => {
     return (
-        <div className="w-full min-h-screen bg-gradient-to-b from-[#121212] to-[#181818] text-white flex flex-col gap-6 p-4">
-            <div className="grid grid-cols-5 grid-rows-5 gap-2 h-full w-full">
-                <div className="row-span-5 border-2 border-gray-200">1</div>
-                <div className="col-span-4 row-span-5 border-2 border-gray-200">2</div>
+        <div className="w-full min-h-screen bg-gradient-to-b from-[#121212] to-[#181818] text-white flex flex-col gap-6 p-4 ">
+            <div className="grid grid-cols-5 grid-rows-5 gap-2 h-screen w-full">
+            <div className="col-start-1 col-end-2 row-start-1 row-end-6 border-2 border-gray-100/20 rounded-3xl p-4 ">1</div>
+            <div className="col-start-2 col-end-6 row-start-1 row-end-6 border-2 border-gray-100/20 rounded-3xl p-4">2</div>
             </div>
         </div>
     )
