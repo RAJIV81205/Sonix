3b8a9cfbba7da98f008906b50fdbb7f19aa1c235
Rajiv <lucky81205@gmail.com>
2025-05-04 01:21:03 +0530

    refactor: simplify fetch request in getNewReleases route by reducing headers and removing unnecessary whitespace

--- app/api/dashboard/getNewReleases/route.ts
+++ app/api/dashboard/getNewReleases/route.ts
@@ -9,47 +9,29 @@ export async function POST(request: Request) {
     if (!user) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-    
 
-    
     const searchUrl = `https://www.jiosaavn.com/api.php?__call=content.getAlbums&api_version=4&_format=json&_marker=0&n=50&p=1&ctx=wap6dot0`;
-    
-    const customHeaders = {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-      "Referer": "https://www.jiosaavn.com/",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.9",
-      "Accept-Encoding": "gzip, deflate, br",
-      "Connection": "keep-alive",
-      "Cache-Control": "max-age=0",
-      "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": '"Windows"',
-      "Sec-Fetch-Site": "same-origin",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-User": "?1",
-      "Sec-Fetch-Dest": "document",
-      "Upgrade-Insecure-Requests": "1",
-      "Cookie": "DL=english; L=english; CT=IN; geo_country=IN; _pl=wap6dot0-"
-    };
-    
+
     const searchResponse = await fetch(searchUrl, {
-      method: "GET",
-      headers: customHeaders,
-      next: { revalidate: 0 },
-      cache: 'no-store'
-    });
-    
+        method: "GET",
+        headers: {
+          "User-Agent": "Mozilla/5.0",
+          "Referer": "https://www.jiosaavn.com/",
+          "Accept": "application/json"
+        }
+      });
+      
+
     if (!searchResponse.ok) {
       return NextResponse.json(
         { error: "Failed to fetch search results" },
         { status: searchResponse.status }
       );
     }
-    
+
     const data = await searchResponse.json();
-    
-    
+    console.log(data)
+
     return NextResponse.json({ data }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json(
