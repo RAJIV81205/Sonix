9d3c62f0949dd76a890bde459fe0ccb68d0e7cc6
Rajiv <lucky81205@gmail.com>
2025-05-15 20:04:10 +0530

    feat: integrate @vercel/speed-insights into layout for performance monitoring; update package.json and package-lock.json to include new dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -50,6 +51,7 @@ export default function RootLayout({
           }}
         />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
