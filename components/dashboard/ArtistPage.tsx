a71832f145e7f613f5609ce412fe784757235c82
Rajiv Dubey <161409149+RAJIV81205@users.noreply.github.com>
2025-07-18 23:09:58 +0530

    feat: implement artist fetching logic in ArtistPage component with error handling and toast notifications

--- components/dashboard/ArtistPage.tsx
+++ components/dashboard/ArtistPage.tsx
@@ -1,11 +1,30 @@
 "use client"
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'next/navigation'
+import { useState } from 'react'
+import toast from 'react-hot-toast'
 
 const ArtistPage = () => {
+    const [artist, setArtist] = useState(null)
+
     const param = useParams().id
-    console.log(param)
+
+    useEffect(() => {
+        const fetchArtist = async () =>{
+            try {
+                
+            } catch (error:any) {
+                console.error("Error fetching artist:", error.message);
+                toast.error("Error fetching artist:", error.message) 
+                
+            }
+        } 
+
+        fetchArtist()
+    }, [param])
+
+
   return (
     <div>ArtistPage {param}</div>
 
